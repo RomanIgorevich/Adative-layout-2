b7a8161c2d36d4628625ec4380187a4d7a7dca75
RomanIgorevich <79832929+RomanIgorevich@users.noreply.github.com>
2021-08-04 18:22:19 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".slider-top__inners").slick({
+    arrows: true,
+    dots: true,
+    autoplay: true,
+    fade: true,
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+  });
+});
